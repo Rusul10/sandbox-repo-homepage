79f40f307efebf586eb77b9d0367cfc41afa1f48
Rusul10 <137110049+Rusul10@users.noreply.github.com>
2023-07-22 20:55:32 +0300

    Initial commit

---
+++ components/Footer.jsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div className="footer">Copywrite Sct 2023</div>;
+}
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import Header from "/components/Header";
+import Homepage from "/components/Homepage";
+import Footer from "/components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <Homepage />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
